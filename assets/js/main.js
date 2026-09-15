92fbfcd13ef6c11f85ab7dd00d6c3f4beca7e236
Lucas <lucas-robinson@hotmail.com>
2026-09-14 23:55:15 -0300

    Modificando a validação de altura

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,26 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
+
     const inputPeso = event.target.querySelector('#peso');
     const inputAltura = event.target.querySelector('#altura');
 
-    const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value);
-    
-    // Se o peso for falso, retorne peso inválido
-    if (!peso) {
-        setResultado('Peso inválido', false);
+    // Verificando se o peso foi preenchido corretamente
+    if (!/^\d+(\.\d+)?$/.test(inputPeso.value)) {
+        setResultado('Peso inválido. Digite um valor válido.', false);
         return;
     }
 
-    // Se a altura for falsa, retorne altura inválida
-    if (!altura) {
-        setResultado('Altura inválida', false);
+    // Verificando se a altura foi preenchida corretamente
+    // A altura precisa obrigatoriamente ter ponto.
+    // Exemplos válidos: 1.70, 1.80, 1.65
+    if (!/^\d+\.\d{1,2}$/.test(inputAltura.value)) {
+        setResultado('Altura inválida. Digite usando ponto, por exemplo: 1.70', false);
         return;
     }
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
 
     // Colhendo a informação e imprimindo a mensagem.
     const imc = getIMC(peso, altura);
@@ -28,43 +31,55 @@ form.addEventListener('submit', function (event) {
     const msg = `Seu IMC é ${imc} (${classificacao}).`;
 
     setResultado(msg, true);
-    // Continua...
 });
 
+
 // Validando a classificação que o usuário se encontra
-function getClassificacao (imc) {
-    const classificacao = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+function getClassificacao(imc) {
+    const classificacao = [
+        'Abaixo do peso',
+        'Peso normal',
+        'Sobrepeso',
+        'Obesidade grau 1',
+        'Obesidade grau 2',
+        'Obesidade grau 3'
+    ];
 
     if (imc >= 39.9) return classificacao[5];
-    
+
     if (imc >= 34.9) return classificacao[4];
-    
+
     if (imc >= 29.9) return classificacao[3];
-    
-    if (imc >= 24.9) return classificacao[2]; 
-    
-    if (imc >= 18.5) return classificacao[1];    
-    
+
+    if (imc >= 24.9) return classificacao[2];
+
+    if (imc >= 18.5) return classificacao[1];
+
     if (imc < 18.5) return classificacao[0];
 }
 
+
 // Função que calcula o peso e altura e coloca em duas casas decimais.
-function getIMC (peso, altura) {
+function getIMC(peso, altura) {
     const imc = peso / altura ** 2;
     return imc.toFixed(2);
 }
 
+
 // Função para criar parágrafo.
-function criaParagrafo () {
+function criaParagrafo() {
     const p = document.createElement('p');
     return p;
 }
 
-// Função que zera a constante resultado, adiciona o texto na constante p e imprime a mensagem na div de resultado do html.
-function setResultado (msg, valida) {
+
+// Função que zera a constante resultado,
+// adiciona o texto na constante p
+// e imprime a mensagem na div de resultado do HTML.
+function setResultado(msg, valida) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
-    
+
     const p = criaParagrafo();
 
     if (valida) {
